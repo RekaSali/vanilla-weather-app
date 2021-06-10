d0cf13974c7c6432aa6099b7701b430936fd377f
RekaSali <84880361+RekaSali@users.noreply.github.com>
2021-06-10 12:16:10 +0100

    Adding axios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "77ddf1d91754306b3e68d959fd8e4d12";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
