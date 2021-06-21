ce152c66a80e75286d61d4e45463c97afac64152
RekaSali <84880361+RekaSali@users.noreply.github.com>
2021-06-21 22:44:08 +0100

    Fixing the forecast row

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ function displayForecast(response) {
 
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 7) {
       forecastHTML =
         forecastHTML +
         `
@@ -55,7 +55,6 @@ ${Math.round(forecastDay.temp.max)}° </span>
   <span class="weather-forecast-temperature-min">
 ${Math.round(forecastDay.temp.min)}° </span>
 
-</div>
 </div>
 </div>
 
