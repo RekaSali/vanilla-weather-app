9cea1bfd0118b14e6864adff0457cb331d45424c
RekaSali <84880361+RekaSali@users.noreply.github.com>
2021-06-23 23:14:37 +0100

    Change from http to https in JS

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ function displayForecast(response) {
   <div class="col-2">
     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
 <img 
-src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
 alt=""
 width="44"
 />
@@ -89,7 +89,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
